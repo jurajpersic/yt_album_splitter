65487e432cad507af2f0cb5487c0f23607d4a360
Juraj Persic <juraj.persic@fer.hr>
2018-12-26 23:57:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # yt_album_splitter
-Python script for downloading and splitting an album from youtube
+Python script for downloading and splitting an album from youtube.
+
+## Prerequisits
+```
+sudo pip install pydub
+sudo pip install youtube_dl
+```
+
+## Usage
+Call python script yt_downloader.py with 2 arguments.
+- First argument is a link to youtube video to be downloaded.
+- Second argument is a file (or path to file) which determines how the mp3 file will be sliced. 
+
+The  text file should contain song titles and starting times of each song in one of the following formats: 
+- hh:mm:ss
+- mm:ss
+
+It works with both leading zeros and without them.
+It is irrelevant whether the title is after of before the start time. 
+The script will create full audio mp3 from the video, as well as individual titled tracks.
+
+## Example
+
+As an example, you can download an album with:
+```
+cd example
+python ../yt_album_splitter.py 'https://www.youtube.com/watch?v=N0LZ20ppkNo' track_list.txt
+```
+Enjoy in good music!
---
+++ yt_album_splitter.py
@@ -0,0 +1,80 @@
+from __future__ import unicode_literals
+import youtube_dl
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import io
+import re
+import sys
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+
+url = str(sys.argv[1])
+text_file = str(sys.argv[2])
+print url
+print text_file
+
+    
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+info = ydl.extract_info(url, download=False)
+ydl.download([url])
+
+full_filename = info['title'] + u'-'+ info['display_id']+'.mp3'
+
+sound = AudioSegment.from_mp3(full_filename)
+
+file = io.open(text_file,'r', encoding='utf-8')
+data = file.read()
+lines = data.split('\n')
+start_times = []
+titles = []
+for i in range(len(lines)):
+    if len(lines[i])>1:
+        split_line = lines[i].split(' ')
+        title_str = ''
+        title_initiated = False
+        start_hour = 0
+        start_min = 0
+        start_sec = 0
+        for j in split_line:
+            split_semicolon = j.split(":")
+            if len(split_semicolon)==2:
+                start_min = int(split_semicolon[0])
+                m = re.search(r'\d+', split_semicolon[1])
+                numeric = m.group()
+                start_sec = int(numeric)
+            elif len(split_semicolon)==3:
+                start_hour= int(split_semicolon[0])
+                start_min = int(split_semicolon[1])
+                m = re.search(r'\d+', split_semicolon[2])
+                numeric = m.group()
+                start_sec = int(numeric)
+            elif not title_initiated:
+                title_str = title_str + split_semicolon[0]
+                title_initiated = True
+            else:
+                title_str = title_str + ' ' 
+                title_str = title_str + split_semicolon[0]
+        if title_str[-1] == u' ':
+            title_str = title_str[0:-1]  
+        print start_hour,start_min,start_sec,title_str
+        start_times = start_times + [start_sec + start_min*60 + start_hour*3600]
+        titles = titles + [title_str]
+print start_times
+print titles
+
+for i in range(len(start_times)-1):
+    start_idx = start_times[i] * 1000
+    end_idx = start_times[i+1] * 1000
+    temp = sound[start_idx:end_idx]
+    temp.export(titles[i]+".mp3", format="mp3")
+start_idx=end_idx
+temp = sound[start_idx:]
+temp.export(titles[-1]+".mp3", format="mp3")
+
